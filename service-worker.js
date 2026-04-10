96e90ddc3068b83479e24564081a398dba0fb98f
nozomi2575 <nozomi2575@g.ecc.u-tokyo.ac.jp>
2026-04-10 13:09:14 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "tree-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./model/model.json",
+  "./model/metadata.json",
+  "./model/weights.bin",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.21.0",
+  "https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js"
+];
+
+// インストール
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// リクエスト処理
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
